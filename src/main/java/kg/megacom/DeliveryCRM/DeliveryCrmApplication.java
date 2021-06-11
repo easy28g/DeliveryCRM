a2a1c547e18dabef685d03e6a9aad0ccda83eba0
Azamat Duisheev <azamat27071998@gmail.com>
2021-06-11 18:21:57 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/DeliveryCRM/DeliveryCrmApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.DeliveryCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/megacom/DeliveryCRM/DeliveryCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.DeliveryCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
